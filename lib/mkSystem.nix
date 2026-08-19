6e23e50612da43466c53f72d9aa2178782ac8180
Ladas552 <me@ladas552.me>
2026-08-19 10:46:14 +0500

    updated recurse search script thanks to llakala

--- lib/mkSystem.nix
+++ lib/mkSystem.nix
@@ -2,14 +2,25 @@
 # it's a small script to make hosts out of modulus
 nixpkgs: pkgs:
 let
-  inherit (pkgs.lib) concatMap hasSuffix filesystem;
-  inherit (builtins) isPath filter readFileType;
-  expandIfFolder =
-    elem:
-    if !isPath elem || readFileType elem != "directory" then
-      [ elem ]
-    else
-      filesystem.listFilesRecursive elem;
+  inherit (pkgs.lib) concatMap hasSuffix hasPrefix;
+  inherit (builtins) attrNames readDir;
+  listNixFilesRecursive =
+    folder:
+    let
+      contents = readDir folder;
+    in
+    concatMap (
+      filename:
+      let
+        type = contents.${filename};
+      in
+      if type == "regular" && hasSuffix ".nix" filename && !hasPrefix "_" filename then
+        [ (folder + "/${filename}") ]
+      else if type == "directory" then
+        listNixFilesRecursive (folder + "/${filename}")
+      else
+        [ ]
+    ) (attrNames contents);
 in
 {
   conditions ? _: true,
@@ -33,17 +44,7 @@ nixosSystem {
           if (module.enable or true) && module ? config && conditions module then module.config else { }
         )
         (
-          filter
-            # Filter out any path that doesn't look like `*.nix`. Don't forget to use
-            # toString to prevent copying paths to the store unnecessarily
-            (
-              path:
-              let
-                pathString = toString path;
-              in
-              !isPath path || hasSuffix ".nix" pathString
-            )
-            # Expand any folder to all the files within it.
-            (concatMap expandIfFolder paths)
+          # Expand any folder to all the files within it.
+          concatMap listNixFilesRecursive paths
         );
 }
